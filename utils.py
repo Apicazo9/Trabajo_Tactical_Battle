cd05b8a3aeee3cf4fcb729274201d04378663e53
rodritl2511 <148353902+rodritl2511@users.noreply.github.com>
2023-10-24 12:54:01 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
+import tablero
 def limpiar_terminal():
 print(chr(27) + "[2J")
-def validar_celda(celda, max_col, max_row) -> bool:
-# Para comprobar si una celda "B5" está dentro es una posición válida del tablero que comprende entre A1 y (max_col, max_row)
+# comprobar si una posicion es valida
+def validar_celda (celda, max_col, max_raw) -> bool:
+    return (celda[0] >= 'a' and celda[0] <= max_col) and (celda[1] >= '1' and celda[1] <= str(max_raw))
+
+
+# comprobar si un miembro del equipo ya ocupa una celda dada
 def comprobar_celda_disponible(celda, equipo) -> bool:
-# Para comprobar si un miembro del equipo ya ocupa una celda dada
-def validar_celda_contigua(celda1, celda2) -> bool:
-# Para comprobar si la celda 1 es contigua a la celda 2
+     return tablero.tablero[celda] == equipo
+
+
+# comprobar celda contigua
+def validar_celda_contigua(celda1, celda2):
+    return (chr(ord(celda1[0]) + 1) == celda2[0] or chr(ord(celda1[0]) - 1) == celda2[0]) or (chr(ord(celda1[1]) + 1) == celda2[1] or chr(ord(celda1[1]) - 1) == celda2[1])
