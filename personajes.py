57be936914327365a3ed705c0e41993e43aa9b5d
Apicazo9 <90835507+Apicazo9@users.noreply.github.com>
2023-10-28 22:00:51 +0200

    Update personajes.py

--- personajes.py
+++ personajes.py
@@ -1,22 +1,57 @@
 from utils import validar_celda, validar_celda_contigua, devolver_area
 class Personaje:
-    def __init__(self, vida, rango, posicion):
-            self.vida_maxima = vida
-            self.vida_actual = vida
-            self.posicion = posicion
-            self.enfriamiento = 0
-            self.rango = rango
+    def __init__(self, vida, rango, nombre):
+        self.nombre = nombre
+        self.vida_maxima = vida
+        self.vida_actual = vida
+        self.posicion = ""
+        self.enfriamiento = 0
+        self.rango = rango
+        self.max_col = 'd'
+        self.max_row = 4
 
-    def mover(self, clase, celda_nueva, max_col, max_row):
-        col = celda_nueva[0]
-        row = celda_nueva[1]
-        if validar_celda(celda_nueva, max_col, max_row) and validar_celda_contigua(col, row):
-            clase.posicion = celda_nueva
+    def mover(self, celda_nueva):
+        self.posicion = celda_nueva
         return None
-    def habilidad(self, atacante, casilla, lista_equipo):
-        lista_afectados = []
-        if validar_celda(casilla, 'd', 4) and (atacante.enfriamineto == 0):
-            if atacante == 'artillero' or atacante == 'inteligencia':
+    def habilidad(self):
+        raise NotImplementedError
+class Medico(Personaje):
+    def __init__(self):
+        super().__init__(1, 'no militar', 'Medico')
+
+    def habilidad(self, personaje):
+        self.curar_personaje(personaje)
+        return None
+    def curar_personaje(self, personaje):
+        personaje.vida_actual = personaje.vida_maxima
+class Artillero(Personaje):
+    def __int__(self):
+        super().__init__(2, 'militar', 'Artillero')
+        self.danyo = 1
+    def habilidad(self, celda):
+        return 'A' + celda
+    def disparo_area(self, personaje):
+        personaje.vida_actual -= 1
+class Francotirador(Personaje):
+    def __int__(self):
+        super().__init__(3, 'militar', 'Francotirador')
+        self.danyo = 3
+    def habilidad(self, celda):
+        return 'F' + celda
+    def disparo_unitario(self, personaje):
+        personaje.vida_actual = 0
+class Inteligencia(Personaje):
+    def __int__(self):
+        super().__init__(3, 'militar', 'Inteligencia')
+    def habilidad(self, celda):
+        return 'I' + celda
+    def explorar_area(self, casilla):
+        #print(f'El {personaje} ha sido avistado {personaje.poscion}'.format(personaje, personaje.poscion))
+
+
+        '''lista_afectados = []
+        if atacante.enfriamineto == 0:
+            if atacante == 'Artillero' or atacante == 'Inteligencia':
                 area = devolver_area(casilla)
                 for personaje in lista_equipo:
                     for celda in area:
@@ -26,7 +61,7 @@ class Personaje:
                         return None
                     else:
                         return lista_afectados
-            else:
+            elif atacante == 'Francotirador':
                 for personaje in lista_equipo:
                     if personaje.posicion == casilla:
                         lista_afectados.append(atacante[0] + casilla)
@@ -34,28 +69,8 @@ class Personaje:
                         return None
                     else:
                         return lista_afectados
+            else:
+                self.curar_personaje()
+                return None
         else:
-            return None
-
-class Medico(Personaje):
-    def __init__(self, posicion):
-        super().__init__(1, 'no militar', posicion)
-    def curar_personaje(self, personaje):
-        personaje.vida_acutal = personaje.vida_maxima
-class Artillero(Personaje):
-    def __int__(self, posicion):
-        super().__init__(2, 'militar', posicion)
-        self.danyo = 1
-    def disparo_area(self, personaje):
-        personaje.vida_acutal -= 1
-class Francotirador(Personaje):
-    def __int__(self, posicion):
-        super().__init__(3, 'militar', posicion)
-        self.danyo = 3
-    def disparo_unitario(self, personaje):
-        personaje.vida_acutal = 0
-class Inteligencia(Personaje):
-    def __int__(self, posicion):
-        super().__init__(3, 'militar', posicion)
-    def explorar_area(self, personaje):
-        print(f'El {personaje} esta en la posicion {personaje.poscion}'.format(personaje, personaje.poscion))
+            return None'''
